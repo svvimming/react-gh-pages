edd4a1132cb51b4bfc9618a4dda6873622155ff5
svvimming <benmunozj@gmail.com>
2020-02-10 11:02:58 -0500

    okieeee

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index00.css';
+import './index.css';
 import Chain from './App';
 import * as serviceWorker from './serviceWorker';
 
