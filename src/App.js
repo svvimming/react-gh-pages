e4be420468ecf013f1ed23b99073c68205ce225f
svvimming <benmunozj@gmail.com>
2020-02-10 00:03:33 -0500

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chain from './Glob.js'
 
 function App() {
   return (
+    <Chain/>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
